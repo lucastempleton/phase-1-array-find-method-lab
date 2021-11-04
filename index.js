ad754d62175d6b7646aea48235ccd745a182b6c3
Lucas Templeton <templetl99@gmail.com>
2021-11-04 10:42:50 -0500

    commit tree 32d33ed689c0ef959f7278f7ac8c3798c717d795

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+// function findWins(wins){
+//     return wins.result === "W"
+// }
+// function superbowlWin(array){
+//     return Object.keys(array).find(findWins)
+// }
+
+// function superbowlWin(object) {
+//     return Object.keys(object).find(key => object[key] === "W");
+//   }
+
+
+// function superbowlWin(array){
+//     let win = array.find(findWins);
+//     return win
+// }
+
+
+// function findWins(array){
+//     return array.year === "W"
+// }
+
+const superbowlWin = array => {
+    const winningObject = array.find(wins => wins.result === "W");
+        if(winningObject === undefined){
+            return undefined
+        }
+        else{
+            return winningObject.year
+        }
+}
 
